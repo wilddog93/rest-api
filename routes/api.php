70ba8a54357cc05ba4e1208573d33f67161bec81
ridhoajibx <ridhoajibx@gmail.com>
2020-05-26 03:55:29 +0700

    create logout api

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Auth')->group(function(){
     Route::post('register', 'RegisterController');
     Route::post('login', 'LoginController');
+    Route::post('logout', 'LogoutController');
 });
  Route::get('user', 'UserController');
