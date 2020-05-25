c74a49bab6a9ceecabf2650cfd200f5995c1c49f
ridhoajibx <ridhoajibx@gmail.com>
2020-05-26 03:46:23 +0700

    create register and login API with jwt

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\User;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        User::create([
+            'name' => request('name'),
+            'username' => request('username'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+
+        return response('Thanks, you are registered.');
+    }
+}
