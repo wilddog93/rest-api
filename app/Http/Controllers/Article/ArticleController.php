f4e9d3e9708ee1e11e0c0b5536cb45d11796b92e
ridhoajibx <ridhoajibx@gmail.com>
2020-05-28 02:08:39 +0700

    delete article

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -73,9 +73,10 @@ class ArticleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Article $article)
     {
-        //
+        $article->delete();
+        return response()->json('The article was delete', 200);
     }
 
     public function articleStore()
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::namespace('Auth')->group(function(){
 Route::namespace('Article')->middleware('auth:api')->group(function(){
     Route::post('create-new-article', 'ArticleController@store');
     Route::patch('edit-article/{article}', 'ArticleController@update');
+    Route::delete('delete-article/{article}', 'ArticleController@destroy');
 });
 Route::get('articles/{article}', 'Article\ArticleController@show');
 Route::get('articles', 'Article\ArticleController@index');
